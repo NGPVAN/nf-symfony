91f64ad5a17d6ca429c34fc62215ff6ef65e0795
Graham Christensen <gchristensen@nationalfield.org>
2012-10-02 16:37:57 -0400

    Breaking out the slave connection retrieval into its own method.

--- lib/plugins/sfPropelPlugin/lib/vendor/propel/Propel.php
+++ lib/plugins/sfPropelPlugin/lib/vendor/propel/Propel.php
@@ -535,29 +535,17 @@ class Propel
 
 			return self::$connectionMap[$name]['master'];
 
-		} else {
-
+        } else {
 			if (!isset(self::$connectionMap[$name]['slave'])) {
 
 				// we've already ensured that the configuration exists, in previous if-statement
-				$slaveconfigs = isset(self::$configuration['datasources'][$name]['slaves']) ? self::$configuration['datasources'][$name]['slaves'] : null;
+                $slaveconfigs = isset(self::$configuration['datasources'][$name]['slaves']) ? self::$configuration['datasources'][$name]['slaves'] : null;
 
 				if (empty($slaveconfigs)) { // no slaves configured for this datasource
 					self::$connectionMap[$name]['slave'] = false;
 					return self::getConnection($name, Propel::CONNECTION_WRITE); // Recurse to get the WRITE connection
-				} else { // Initialize a new slave
-					if (isset($slaveconfigs['connection']['dsn'])) { // only one slave connection configured
-						$conparams = $slaveconfigs['connection'];
-					} else {
-						$randkey = array_rand($slaveconfigs['connection']);
-						$conparams = $slaveconfigs['connection'][$randkey];
-						if (empty($conparams)) {
-							throw new PropelException('No connection information in your runtime configuration file for SLAVE ['.$randkey.'] to datasource ['.$name.']');
-						}
-					}
-
-					// initialize master connection
-					$con = Propel::initConnection($conparams, $name);
+                } else { // Initialize a new slave
+                    $con = self::getAvailableSlaveConnection($name, $slaveconfigs);
 					self::$connectionMap[$name]['slave'] = $con;
 				}
 
@@ -567,7 +555,28 @@ class Propel
 
 		} // if mode == CONNECTION_WRITE
 
-	} // getConnection()
+    } // getConnection()
+
+    protected static function getAvailableSlaveConnection($name, $configs)
+    {
+        if (isset($configs['connection']['dsn'])) { // only one slave connection configured
+            $servers = array(
+                'single' => $configs['connection']
+            );
+        } else {
+            $servers = $configs['connection'];
+            shuffle($servers);
+        }
+
+        $conparams = reset($servers);
+        if (empty($conparams)) {
+            throw new PropelException('No connection information in your runtime configuration file for SLAVE ['.$randkiey.'] to datasource ['.$name.']');
+        }
+
+        // initialize master connection
+        $con = Propel::initConnection($conparams, $name);
+        return $con;
+    }
 
 	/**
 	 * Opens a new PDO connection for passed-in db name.
